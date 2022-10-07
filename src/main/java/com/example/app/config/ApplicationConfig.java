01fc4af1d12a7b3c136acfaf68bb47b5bb5677e6
zd2L10 <airrideship556@gmail.com>
2022-10-07 11:45:54 +0900

    ログイン認証用フィルタ作成

--- src/main/java/com/example/app/config/ApplicationConfig.java
+++ src/main/java/com/example/app/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.example.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,12 @@ import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.example.app.filter.AuthFilter;
+
 @Configuration
-public class ValidationConfig implements WebMvcConfigurer {
+public class ApplicationConfig implements WebMvcConfigurer {
 	
+	// バリデーションのメッセージ指定
 	@Override
 	public Validator getValidator() {
 		var validator = new LocalValidatorFactoryBean();
@@ -24,4 +28,12 @@ public class ValidationConfig implements WebMvcConfigurer {
 		messageSource.setBasename("validation");
 		return messageSource;
 	}
+	
+	
+	  // 認証用フィルタ有効化
+	  
+	  @Bean public FilterRegistrationBean<AuthFilter> authFilter(){ var bean = new
+	  FilterRegistrationBean<AuthFilter>(new AuthFilter());
+	  bean.addUrlPatterns("/result/*"); return bean; }
+	 
 }
